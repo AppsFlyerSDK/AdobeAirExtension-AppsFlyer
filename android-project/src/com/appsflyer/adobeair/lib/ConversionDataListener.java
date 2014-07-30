c975ff71bc99a41c78d18b07eb6a5a6336bb86c5
Maksym Gerasymchuk <maxgerasimchuk@gmail.com>
2014-07-30 12:32:24 +0300

    delete unnecessary files

--- android-project/src/com/appsflyer/adobeair/lib/ConversionDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.appsflyer.adobeair.lib;
-
-import java.util.Map;
-
-public interface ConversionDataListener {
-    void onConversionDataLoaded(Map<String,String> conversionData);
-    void onConversionFailure(String errorMessage);
-}
--- android-project/src/com/appsflyer/adobeair/lib/LogMessages.java
+++
@@ -1,9 +0,0 @@
-package com.appsflyer.adobeair.lib;
-
-/**
- * used also to monitor logs in the monitor app
- */
-public class LogMessages {
-    public static final String START_LOG_MESSAGE = "Start tracking package: ";
-
-}
