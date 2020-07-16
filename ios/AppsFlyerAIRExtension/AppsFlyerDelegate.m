75496c4cb6646c20a33343c9362db83645d49fa7
Vitaly Sokolov <vitaly.sokolov@appsflyer.com>
2020-07-16 14:25:58 +0300

    WIP: update iOS callbacks for SDK 5.x.x

--- ios/AppsFlyerAIRExtension/AppsFlyerDelegate.m
+++ ios/AppsFlyerAIRExtension/AppsFlyerDelegate.m
@@ -8,10 +8,10 @@
 
 @synthesize ctx;
 
-- (void) onConversionDataReceived:(NSDictionary*) installData
+- (void) onConversionDataSuccess:(NSDictionary*) conversionInfo
 {
     NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:installData
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:conversionInfo
                                                        options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
                                                          error:&error];
     
@@ -23,26 +23,25 @@
     }
 }
 
+- (void)onConversionDataFail:(NSError *)error
+{
+    NSDictionary *userInfo = [error userInfo];
+    NSString *errorString = [[userInfo objectForKey:NSUnderlyingErrorKey] localizedDescription];
+    NSLog(@"The error is: %@", errorString);
+    dispatchStatusEvent(ctx, @"installConversionFailure", error.localizedDescription);
+}	}
+
 - (void) onAppOpenAttribution:(NSDictionary*) installData {
     NSError *error;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:installData
                                                        options:NSJSONWritingPrettyPrinted
                                                          error:&error];
-    
     if (! jsonData) {
         NSLog(@"Got an error: %@", error);
     } else {
         NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
         dispatchStatusEvent(ctx,@"appOpenAttribution",jsonString);
     }
-}
-
-- (void) onConversionDataRequestFailure:(NSError *)error
-{
-    NSDictionary *userInfo = [error userInfo];
-    NSString *errorString = [[userInfo objectForKey:NSUnderlyingErrorKey] localizedDescription];
-    NSLog(@"The error is: %@", errorString);
-    dispatchStatusEvent(ctx, @"installConversionFailure", error.localizedDescription);
 }
 
 - (void) onAppOpenAttributionFailure:(NSError *)error
