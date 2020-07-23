f572443db0719e56caae64017137880559896324
Vitaly Sokolov <vitaly.sokolov@appsflyer.com>
2020-07-23 15:07:58 +0300

    revert setExtension iOS, update binaries

--- ios/AppsFlyerAIRExtension/AppsFlyerAIRExtension.m
+++ ios/AppsFlyerAIRExtension/AppsFlyerAIRExtension.m
@@ -569,7 +569,6 @@ void AFExtensionInitializer(void** extDataToSet, FREContextInitializer* ctxIniti
 
 void AFExtensionFinalizer(FREContext ctx)
 {
-    [AppsFlyerTracker sharedTracker] setExtension
     return;
 }
 
