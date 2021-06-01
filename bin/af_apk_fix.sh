84b1c0a98ca977c2ba516dc27ebf07e7a4c22814
Vitaly Sokolov <vitaly.sokolov@appsflyer.com>
2021-06-01 11:43:21 +0300

    Android 6.3.1 update

--- bin/af_apk_fix.sh
+++ bin/af_apk_fix.sh
@@ -25,7 +25,7 @@ read -p 'Enter the name of they key from the keystore above that should be used 
 mkdir tmp; 
 
 # Download relecant SDK version for further exctraction of needed files
-curl -o ./tmp/android_sdk.zip 'https://repo.maven.apache.org/maven2/com/appsflyer/af-android-sdk/6.2.3/af-android-sdk-6.2.3.aar';
+curl -o ./tmp/android_sdk.zip 'https://repo.maven.apache.org/maven2/com/appsflyer/af-android-sdk/6.3.1/af-android-sdk-6.3.1.aar';
 
 # Extract jar from the aar
 unzip ./tmp/android_sdk.zip -d tmp/android_sdk;
